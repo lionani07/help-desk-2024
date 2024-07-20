b4574d46c20046ed4b2e29e8b07e977d75a9b981
lcsanchez <lionani07@gmail.com>
2024-07-20 18:59:36 -0300

    perfil de dev

---
+++ src/main/java/br/com/lionani07/helpdesk/config/ProfilesDevConfig.java
@@ -0,0 +1,27 @@
+package br.com.lionani07.helpdesk.config;
+
+import br.com.lionani07.helpdesk.services.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class ProfilesDevConfig {
+
+    @Autowired
+    private DbService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String create;
+
+    @Bean
+    public String initDB() {
+        if ("create".equals(create)) {
+            this.dbService.initDB();
+        }
+        return "Db iniciado";
+    }
+}
